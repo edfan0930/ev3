7c1a8bfe1a725297cb93e559eef80ea6a86ae7de
EdFan <arlerwork@gmail.com>
2018-11-30 23:45:03 +0800

    add vue-router

--- src/renderer/main.js
+++ src/renderer/main.js
@@ -1,12 +1,12 @@
 import Vue from 'vue'
-
+import VueRouter from 'vue-router';
 import App from './App'
 import store from './store'
 import iView from 'iview';
 import 'iview/dist/styles/iview.css';
 
 Vue.config.productionTip = false
-
+Vue.use(VueRouter)
 /* eslint-disable no-new */
 new Vue({
   components: { App },
